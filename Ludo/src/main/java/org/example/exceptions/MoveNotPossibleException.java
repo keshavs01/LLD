477fa1a23087b489fefaafa1a91607e16de04d4e
Keshav Singh <01keshavsinghs.mail@gmail.com>
2023-01-07 23:34:59 +0530

    delete

--- Ludo/src/main/java/org/example/exceptions/MoveNotPossibleException.java
+++
@@ -1,8 +0,0 @@
-package org.example.exceptions;
-
-public class MoveNotPossibleException extends Exception{
-
-    public MoveNotPossibleException(String message){
-        super(message);
-    }
-}
